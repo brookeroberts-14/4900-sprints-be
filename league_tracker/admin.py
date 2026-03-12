77b97b4647d42ee168516e0ecf28fc26201a8ffc
ProAverageJoe <joshnord37@gmail.com>
2026-03-12 12:49:33 -0500

    registering models for the admin page

--- league_tracker/admin.py
+++ league_tracker/admin.py
@@ -15,40 +15,40 @@ class LeagueList(admin.ModelAdmin):
     ordering = ['name']
 
 class DeckList(admin.ModelAdmin):
-    list_display = ('pk', 'name', 'players_per_match', 'rounds_per_match')
-    list_filter = ('name', 'players_per_match', 'rounds_per_match')
-    search_fields = 'name'
+    list_display = ('pk', 'name', 'league_player', 'url')
+    list_filter = ('name', 'league_player')
+    search_fields = ('name', 'league_player')
     ordering = ['name']
 
 class League_PlayerList(admin.ModelAdmin):
-    list_display = ('customer', 'category', 'description', 'recent_value')
-    list_filter = ('customer', 'category')
-    search_fields = ('customer', 'category')
-    ordering = ['customer']
+    list_display = ('league', 'player', 'league_player_points')
+    list_filter = ('league', 'player')
+    search_fields = ('league', 'player')
+    ordering = ['league']
 
 class MatchList(admin.ModelAdmin):
-    list_display = ('customer','symbol', 'name', 'shares', 'purchase_price')
-    list_filter = ('customer','symbol', 'name')
-    search_fields = ('customer','symbol', 'name')
-    ordering = ['customer']
+    list_display = ('league','number', 'status')
+    list_filter = ('league','number', 'status')
+    search_fields = ('league','number', 'status')
+    ordering = ['league']
 
 class Match_Player_DetailList(admin.ModelAdmin):
-    list_display = ('pk', 'name', 'players_per_match', 'rounds_per_match')
-    list_filter = ('name', 'players_per_match', 'rounds_per_match')
-    search_fields = 'name'
-    ordering = ['name']
+    list_display = ('pk', 'match', 'league_player', 'deck')
+    list_filter = ('match', 'league_player')
+    search_fields = ('match', 'league_player', 'deck')
+    ordering = ['match']
 
 class Match_RoundList(admin.ModelAdmin):
-    list_display = ('customer', 'category', 'description', 'recent_value')
-    list_filter = ('customer', 'category')
-    search_fields = ('customer', 'category')
-    ordering = ['customer']
+    list_display = ('match', 'number', 'status')
+    list_filter = ('match', 'number', 'status')
+    search_fields = ('match', 'number', 'status')
+    ordering = ['match']
 
 class Match_Round_PlayerList(admin.ModelAdmin):
-    list_display = ('customer','symbol', 'name', 'shares', 'purchase_price')
-    list_filter = ('customer','symbol', 'name')
-    search_fields = ('customer','symbol', 'name')
-    ordering = ['customer']
+    list_display = ('round','player', 'result', 'points')
+    list_filter = ('round','player', 'result')
+    search_fields = ('round','player', 'result')
+    ordering = ['round']
 
 
 admin.site.register(Format, FormatList)
